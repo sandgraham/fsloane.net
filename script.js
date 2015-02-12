b28f7e501a9fa63ae462783c9ef9f04dd55553ab
sandgraham <grahammurdoch8@gmail.com>
2015-02-11 22:14:59 -0800

    restructure to use yt api and change images

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-function onPlayerReady () {
-    console.log("ready")
+// Load API asynchronously
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+
+var scriptTag = document.getElementsByTagName('script')[0];
+scriptTag.parentNode.insertBefore(tag, scriptTag);
+
+// Set up shitty globals
+var virgin = true, index = Math.floor( Math.random()*92 );
+
+// Create Player
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        playerVars: {
+            list: 'PL3IfWK8YCzkdgmpvUPFBW6kxl92oOZLZb',
+            listType: 'playlist',
+            controls: '2',
+            showinfo: '1',
+            autoplay: '1',
+            autohide: '1',
+            loop: '1',
+            rel: '0',
+            index: index
+        },
+        events: {
+            onStateChange: onPlayerStateChange
+        }
+    })
+}
+
+function onPlayerStateChange (event) {
+    if (event.data === -1 && virgin) {
+        player.setShuffle(true);
+        virgin = false;
+    }
 }
